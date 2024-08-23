4210ba80fb5c3034a926d7dea7d88060fa08ff78
Isabela Vieira <isa.bonham@gmail.com>
2024-08-23 17:58:44 -0300

    finishing 01

--- trabalho_final/backend/src/index.ts
+++ trabalho_final/backend/src/index.ts
@@ -1,14 +1,12 @@
-import express, { Request, Response } from 'express';
+import express from 'express'
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.json());
+const app = express ()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<html> <center> <h1> Loja de CDs <h1> <h3>Este projeto é uma loja destinada a venda de CDs de artistas de vários ritmos e estilos musicais.<h3> <h2> Dupla: Isabela e Bianca <h2></center></html>');
-});
+    res.send('<html> <center> <h1> Loja de CDs <h1> <h3>Este projeto é uma loja destinada a venda de CDs de artistas de vários ritmos e estilos musicais.<h3> <h2> Dupla: Isabela e Bianca <h2></center></html>');
+  });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+    console.log(`Servidor executando na port ${port} em http://localhost:${port}`)
+}) 
