2a0fd0eaa2085c29e3a08d7d51f6acdfb0dfb61b
Isabela Vieira <isa.bonham@gmail.com>
2024-08-24 15:05:18 -0300

    installing dependencies

--- trabalho_final/backend/src/index.ts
+++ trabalho_final/backend/src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
+import userRoutes from './routes/userRoutes'
 
 const app = express ()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('<html> <center> <h1> Loja de CDs <h1> <h3>Este projeto é uma loja destinada a venda de CDs de artistas de vários ritmos e estilos musicais.<h3> <h2> Dupla: Isabela e Bianca <h2></center></html>');
-});
+//app.get('/', (req, res) => {
+//    res.send('<html> <center> <h1> Loja de CDs <h1> <h3>Este projeto é uma loja destinada a venda de CDs de artistas de vários ritmos e estilos musicais.<h3> <h2> Dupla: Isabela e Bianca <h2></center></html>');
+//});
 
 app.use(express.json())
+app.use('/', userRoutes)
 
 app.listen(port, () => {
     console.log(`Servidor executando na port ${port} em http://localhost:${port}`)
